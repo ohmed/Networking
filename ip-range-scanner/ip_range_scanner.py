4e30067c4ff3d65016ca39ce75fa2f86eb93b56e
Oduor Mike <87503468+ohmed@users.noreply.github.com>
2023-06-09 06:43:31 +0000

    format

--- ip-range-scanner/ip_range_scanner.py
+++ ip-range-scanner/ip_range_scanner.py
@@ -1,5 +1,6 @@
 import socket
 
+
 def get_public_ip_domain(ip_address):
 
     try:
@@ -9,15 +10,17 @@ def get_public_ip_domain(ip_address):
     except socket.herror:
         return None
 
+
 def scan_ip_range(start_ip, end_ip):
     available_ips = []
-    for i in range(start_ip, end_ip+1):
+    for i in range(start_ip, end_ip + 1):
         ip_address = f"192.168.0.{i}"
         domain_name = get_public_ip_domain(ip_address)
         if domain_name:
             available_ips.append((ip_address, domain_name))
     return available_ips
-    
+
+
 start_ip = 1
 end_ip = 255
 results = scan_ip_range(start_ip, end_ip)
